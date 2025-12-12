c6085775869466e4812c7377edd790a782a5fc6b
Frank Chen <frank.chen021@outlook.com>
2025-12-12 23:15:15 +0800

    add node metrics

--- src/components/node-tab/node-tab.tsx
+++ src/components/node-tab/node-tab.tsx
@@ -1,9 +1,10 @@
 import DashboardContainer from "@/components/shared/dashboard/dashboard-container";
 import type {
-  Dashboard, DashboardGroup,
+  Dashboard,
+  DashboardGroup,
   StatDescriptor,
   TableDescriptor,
-  TimeseriesDescriptor
+  TimeseriesDescriptor,
 } from "@/components/shared/dashboard/dashboard-model";
 import { OpenDatabaseTabButton } from "@/components/table-tab/open-database-tab-button";
 import { OpenTableTabButton } from "@/components/table-tab/open-table-tab-button";
@@ -709,6 +710,567 @@ SELECT 1 - (sum(free_space) / sum(total_space)) FROM clusterAllReplicas('{cluste
   } as StatDescriptor,
 ];
 
+const overviewMetricsDashboard = [
+  //
+  // Queries/second
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "Queries/second",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(ProfileEvent_Query) AS query_qps
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // CPU Usage (cores)
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "CPU Usage (cores)",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(ProfileEvent_OSCPUVirtualTimeMicroseconds) / 1000000 AS cpu_cores
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // Queries Running
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "Queries Running",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(CurrentMetric_Query) AS queries_running
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // Merges Running
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "Merges Running",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(CurrentMetric_Merge) AS merges_running
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // Selected Bytes/second
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "Selected Bytes/second",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    fieldOptions: {
+      selected_bytes: {
+        format: "binary_size",
+      },
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(ProfileEvent_SelectedBytes) AS selected_bytes
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // IO Wait
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "IO Wait",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(ProfileEvent_OSIOWaitMicroseconds) / 1000000 AS io_wait
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // CPU Wait
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "CPU Wait",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(ProfileEvent_OSCPUWaitMicroseconds) / 1000000 AS cpu_wait
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // OS CPU Usage (Userspace)
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "OS CPU Usage (Userspace)",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(value) AS OSUserTimeNormalized
+FROM merge('system', '^asynchronous_metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+  AND metric = 'OSUserTimeNormalized'
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // OS CPU Usage (Kernel)
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "OS CPU Usage (Kernel)",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(value) AS OSSystemTimeNormalized
+FROM merge('system', '^asynchronous_metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+  AND metric = 'OSSystemTimeNormalized'
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // Read From Disk
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "Read From Disk",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    fieldOptions: {
+      OSReadBytes: {
+        format: "binary_size",
+      },
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(ProfileEvent_OSReadBytes) AS OSReadBytes
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // Read From Filesystem
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "Read From Filesystem",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(ProfileEvent_OSReadChars) AS OSReadChars
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // Memory (tracked)
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "Memory (tracked)",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    fieldOptions: {
+      memory_tracking_bytes: {
+        format: "binary_size",
+      },
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(CurrentMetric_MemoryTracking) AS memory_tracking_bytes
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // In-Memory Caches (bytes)
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "In-Memory Caches (bytes)",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    fieldOptions: {
+      cache_bytes: {
+        format: "binary_size",
+      },
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  arraySum([COLUMNS('CurrentMetric_.*CacheBytes') EXCEPT 'CurrentMetric_FilesystemCache.*' APPLY avg]) AS cache_bytes
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // Load Average (15 minutes)
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "Load Average (15 minutes)",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(value) AS LoadAverage15
+FROM merge('system', '^asynchronous_metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+  AND metric = 'LoadAverage15'
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // Selected Rows/second
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "Selected Rows/second",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(ProfileEvent_SelectedRows) AS selected_rows_per_second
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // Inserted Rows/second
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "Inserted Rows/second",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(ProfileEvent_InsertedRows) AS inserted_rows_per_second
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // Total MergeTree Parts
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "Total MergeTree Parts",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  avg(value) AS TotalPartsOfMergeTreeTables
+FROM merge('system', '^asynchronous_metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+  AND metric = 'TotalPartsOfMergeTreeTables'
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // Max Parts For Partition
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "Max Parts For Partition",
+      align: "center",
+    },
+    width: 6,
+    legendOption: {
+      placement: "none",
+      values: ["min", "max", "last"],
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  max(value) AS MaxPartCountForPartition
+FROM merge('system', '^asynchronous_metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+  AND metric = 'MaxPartCountForPartition'
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+
+  //
+  // Concurrent network connections
+  //
+  {
+    type: "line",
+    titleOption: {
+      title: "Concurrent network connections",
+      align: "center",
+    },
+    width: 6,
+    tooltipOption: {
+      sortValue: "desc",
+    },
+    query: {
+      sql: `
+SELECT
+  toStartOfInterval(event_time, INTERVAL {rounding:UInt32} SECOND)::INT as t,
+  max(CurrentMetric_TCPConnection) AS TCP_Connections,
+  max(CurrentMetric_MySQLConnection) AS MySQL_Connections,
+  max(CurrentMetric_HTTPConnection) AS HTTP_Connections,
+  max(CurrentMetric_InterserverConnection) AS Interserver_Connections
+FROM merge('system', '^metric_log')
+WHERE event_date >= toDate(now() - {seconds:UInt32})
+  AND event_time >= now() - {seconds:UInt32}
+GROUP BY t
+ORDER BY t WITH FILL STEP {rounding:UInt32}
+`,
+    },
+  } as TimeseriesDescriptor,
+];
+
 const clusterMetricsDashboard = [
   //
   // Insert Queries Per Second
@@ -912,21 +1474,29 @@ const NodeTabComponent = (_props: NodeTabProps) => {
     ],
   } as Dashboard;
 
-  if (
+  const isClusterMode =
     connection &&
     connection.selectedConnection &&
     connection.selectedConnection.cluster &&
-    connection.selectedConnection.cluster.length > 0
-  ) {
+    connection.selectedConnection.cluster.length > 0;
+  if (isClusterMode) {
     dashboard.charts.push({
       title: "Cluster Status",
       collapsed: false,
       charts: clusterStatusDashboard,
     } as DashboardGroup);
+  }
 
+  dashboard.charts.push({
+    title: "Node Overview Metrics",
+    collapsed: false,
+    charts: overviewMetricsDashboard,
+  } as DashboardGroup);
+
+  if (isClusterMode) {
     dashboard.charts.push({
       title: "Cluster Metrics",
-      collapsed: false,
+      collapsed: true,
       charts: clusterMetricsDashboard,
     } as DashboardGroup);
   }
